4d405fdda85a74155c056dffe3ec6e79eb113009
xudafeng <xudafeng@126.com>
2016-06-03 15:40:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# macaca-electron-screenshot
+
+```shell
+$ node index.js
+```
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2015 xdf
---
+++ test/macaca-electron-screenshot.test.js
@@ -0,0 +1,20 @@
+/* ================================================================
+ * macaca-electron-screenshot by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Fri Jun 03 2016 15:29:04 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+var macaca-electron-screenshot = require('..');
+
+describe('test', function() {
+
+});
